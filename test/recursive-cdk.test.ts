20cc91476a10467b7ee7bf70d2c238346216b823
yamatatsu <the.kinnikuman@gmail.com>
2023-12-04 21:38:28 +0900

    Initial commit

---
+++ test/recursive-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RecursiveCdk from '../lib/recursive-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/recursive-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RecursiveCdk.RecursiveCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
